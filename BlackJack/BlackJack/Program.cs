a7168f60889048bdfce5da795b819c83f7cd48c6
Jonathan Hartman <jonhtii@aol.com>
2015-03-19 15:54:10 -0400

    Initial Commit
    Added stylcop restrictions and comments, as well as the Drawing and
    OpenTK libraries

---
+++ BlackJack/BlackJack/Program.cs
@@ -0,0 +1,18 @@
+﻿// <summary> Program entry point. </summary>
+
+namespace BlackJack
+{
+    /// <summary>
+    /// Class to hold the entry point.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Creates a window that will run until the user closes it.
+        /// </summary>
+        /// <param name="args">Parameter not used.</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
