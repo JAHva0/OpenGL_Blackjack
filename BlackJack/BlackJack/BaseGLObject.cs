cc45099e9eab44c96279096812b7f6d13ef680c0
JAHva0 <JAHva01@gmail.com>
2015-03-19 21:50:30 -0400

    Stared Mesh Class
    Now parses .obj files into lists. Still need to store the data in a
    usable format.

--- BlackJack/BlackJack/BaseGLObject.cs
+++ BlackJack/BlackJack/BaseGLObject.cs
@@ -7,9 +7,15 @@ namespace BlackJack
     /// </summary>
     public class BaseGLObject
     {
+        /// <summary> The handle for the vertex buffer data. </summary>
+        private int vertexBufferObject = -1;
+
+        /// <summary> The handle for the index buffer data. </summary>
+        private int indexBufferObject = -1;
+        
         /// <summary>
         /// Initializes a new instance of the <see cref="BaseGLObject"/> class. 
-        /// The blank constructor just creates a basic textured cube for testing.
+        /// The blank constructor just creates a basic card for testing.
         /// </summary>
         public BaseGLObject()
         {
---
+++ BlackJack/BlackJack/Mesh.cs
@@ -0,0 +1,87 @@
+﻿namespace BlackJack
+{
+    using System;
+    using System.Collections.Generic;
+    using OpenTK;
+
+    
+    public class Mesh
+    {
+        private List<Vector3> vertexList = new List<Vector3>();
+        private List<Vector2> textureList = new List<Vector2>();
+        private List<Vector3> normalList = new List<Vector3>();
+
+        private List<int[]> faceList = new List<int[]>();
+
+        public Mesh(string modeldata)
+        {
+            foreach (string line in modeldata.Split('\n'))
+            {
+                this.ParseLine(line);
+            }
+        }
+
+        private void ParseLine(string line)
+        {
+            if (line != string.Empty)
+            {
+                string type = line.Substring(0, 2);
+                string data = line.Substring(2);
+                switch (type)
+                {
+                    case "o ":
+                        {
+                            Console.WriteLine("Loading Object " + data + "...");
+                            break;
+                        }
+
+                    case "v ":
+                        {
+                            Vector3 newVector = new Vector3();
+                            float.TryParse(data.Split(' ')[0], out newVector.X);
+                            float.TryParse(data.Split(' ')[1], out newVector.Y);
+                            float.TryParse(data.Split(' ')[2], out newVector.Z);
+                            this.vertexList.Add(newVector);
+                            break;
+                        }
+
+                    case "vt":
+                        {
+                            Vector2 newTexVert = new Vector2();
+                            float.TryParse(data.Split(' ')[1], out newTexVert.X);
+                            float.TryParse(data.Split(' ')[2], out newTexVert.Y);
+                            this.textureList.Add(newTexVert);
+                            break;
+                        }
+
+                    case "vn":
+                        {
+                            Vector3 newNormal = new Vector3();
+                            float.TryParse(data.Split(' ')[0], out newNormal.X);
+                            float.TryParse(data.Split(' ')[1], out newNormal.Y);
+                            float.TryParse(data.Split(' ')[2], out newNormal.Z);
+                            this.normalList.Add(newNormal);
+                            break;
+                        }
+
+                    case "f ":
+                        {
+                            foreach (string subData in data.Split(' '))
+                            {
+                                int vertFace, texFace, normFace;
+                                int.TryParse(subData.Split('/')[0], out vertFace);
+                                int.TryParse(subData.Split('/')[1], out texFace);
+                                int.TryParse(subData.Split('/')[2], out normFace);
+                                vertFace--;
+                                texFace--;
+                                normFace--;
+                                this.faceList.Add(new int[] { vertFace, texFace, normFace });
+                            }
+
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
